da231d8ad21fb770925d8814719529e56ff5a04e
Madeline <mad2fleury@gmail.com>
2025-08-02 20:08:32 +0200

    fixes

--- code/scripts/PhotosDisplay2.gd
+++ code/scripts/PhotosDisplay2.gd
@@ -1,9 +1,9 @@
 extends Node
 
 @export var yMin : float = 0
-@export var yMax : float = 0.55
-@export var scaleClose : float = 1.
-@export var scaleFar : float = 0.7
+@export var yMax : float = 1
+@export var scaleClose : float = 5.
+@export var scaleFar : float = 0.5
 
 func printPhoto(size : Vector2, items : Array[Sprite2D], parent_scale: float):
 	#background.position = Vector2(0,0)
@@ -17,9 +17,12 @@ func printPhoto(size : Vector2, items : Array[Sprite2D], parent_scale: float):
 	for item in items:
 		if (item.position.x > 0 and item.position.x < 1 and 
 			item.position.y > 0 and item.position.y < 1):
-			var scale = (-4.5 * item.position.y + 5) * parent_scale
-			item.position.x = item.position.x * size.x - 0.5 * size.x
-			item.position.y = (1 -item.position.y) * (yMax - yMin) * size.y - 0.5 * size.y
+			print(item.texture.resource_path)
+			print(item.position)
+			var scale = ((scaleFar - scaleClose) * item.position.y + scaleClose) * parent_scale
+			item.position.x = item.position.x * size.x
+			item.position.y = (1 -item.position.y) * (yMax - yMin) * size.y
+			print(item.position)
 			item.scale = Vector2(scale, scale)
 			sub_items.append(item)
 	return sub_items
@@ -39,7 +42,12 @@ func _ready():
 		var size = Vector2(sprite.texture.get_width() * sprite.scale.x, sprite.texture.get_height() * sprite.scale.y)
 		var new_items = printPhoto(size, items, sprite.scale.y)
 		for item in new_items:
-			item.position = item.position + sprite.position #+ 0.5 * size
+			item.position += sprite.position + Vector2(-0.5 * size[0], -0.5 * size[1])
+			item.position.y *= 1.2 #hack
+			print(item.texture.resource_path)
+			print(size)
+			print(sprite.position)
+			print(item.position)
 			item.z_index = z_index
 			add_child(item)	
 		z_index += 1
