7085814c2f4c9539dd9f643e0a3269542be9bb5a
Madeline <mad2fleury@gmail.com>
2025-08-03 10:01:00 +0200

    feat: add table and rug to background of photos

--- code/scripts/intro.gd
+++ code/scripts/intro.gd
@@ -5,7 +5,6 @@ signal skip
 
 var index = 0
 var vnodes : Array[Node] = []
-var ispressed : bool = false
 
 func _ready():
 	index = 0
@@ -21,15 +20,12 @@ func next():
 		vnodes[index].show()
 	index += 1
 
-#func _process(_delta: float) -> void:
-	#if Input.is_anything_pressed():
-	#	if ispressed == false:
-	#		ispressed = true
-	#		if (index < vnodes.size()):
-	#			next()
-	#		else:
-	#			skip.emit()
-	#S	ispressed = false
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("main_action"):
+		if (index < vnodes.size()):
+			next()
+		else:
+			skip.emit()
 
 
 func _on_button_button_up() -> void:
